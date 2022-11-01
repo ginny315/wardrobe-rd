6f3a4d93d75388a829fc4063a9df843d6157941c
ginny <gny315@163.com>
2022-11-01 17:01:49 +0800

    update

--- db.js
+++ db.js
@@ -1,32 +1,51 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 // 从环境变量中读取数据库配置
-const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+// const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+const MYSQL_USERNAME = "root"
+const MYSQL_PASSWORD = "qDEDJGA6"
+const MYSQL_ADDRESS = "sh-cynosdbmysql-grp-d26a3biu.sql.tencentcdb.com:28035"
 
 const [host, port] = MYSQL_ADDRESS.split(":");
 
-const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
+const sequelize = new Sequelize("wardrobe", MYSQL_USERNAME, MYSQL_PASSWORD, {
   host,
   port,
   dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
 });
 
 // 定义数据模型
-const Counter = sequelize.define("Counter", {
-  count: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 1,
+const Clothes = sequelize.define("cloth", {
+  id:{
+    type:Sequelize.INTEGER,  //数据类型
+    primaryKey:true,  //是否为主键
+    unique:true,   //是否唯一
+    autoIncrement:true  //自增
+  },
+  season: {
+    type: DataTypes.STRING,
+  },
+  type: {
+    type: DataTypes.STRING,
+  },
+  position: {
+    type: DataTypes.STRING,
+  },
+  other: {
+    type: DataTypes.STRING,
+  },
+  imgurl: {
+    type: DataTypes.STRING,
   },
 });
 
 // 数据库初始化方法
 async function init() {
-  await Counter.sync({ alter: true });
+  await Clothes.sync({ alter: true });
 }
 
 // 导出初始化方法和模型
 module.exports = {
   init,
-  Counter,
+  Clothes,
 };
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const logger = require("koa-logger");
 const bodyParser = require("koa-bodyparser");
 const fs = require("fs");
 const path = require("path");
-const { init: initDB, Counter } = require("./db");
-
+const { init: initDB, Clothes } = require("./db");
+const cloud = require('wx-server-sdk');
 const router = new Router();
+const cors = require('koa2-cors');
 
 const homePage = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
 
@@ -16,26 +17,26 @@ router.get("/", async (ctx) => {
 });
 
 // 更新计数
-router.post("/api/count", async (ctx) => {
-  const { request } = ctx;
-  const { action } = request.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
+// router.post("/api/count", async (ctx) => {
+//   const { request } = ctx;
+//   const { action } = request.body;
+//   if (action === "inc") {
+//     await Counter.create();
+//   } else if (action === "clear") {
+//     await Counter.destroy({
+//       truncate: true,
+//     });
+//   }
 
-  ctx.body = {
-    code: 0,
-    data: await Counter.count(),
-  };
-});
+//   ctx.body = {
+//     code: 0,
+//     data: await Counter.count(),
+//   };
+// });
 
 // 获取计数
-router.get("/api/count", async (ctx) => {
-  const result = await Counter.count();
+router.get("/api/get_clothes", async (ctx) => {
+  const result = await Clothes.findAll({});
 
   ctx.body = {
     code: 0,
@@ -50,14 +51,39 @@ router.get("/api/wx_openid", async (ctx) => {
   }
 });
 
+router.post("/api/add_clothes", async (ctx) => {
+  console.log(111)
+  const { body } = ctx.request;
+  console.log('upload body=', body,)
+  const item = {
+    season: body.season[0],
+    type: body.type[0],
+    position: body.position[0],
+    other: body.other,
+    imgurl: body.imgurl
+  }
+  const result = await Clothes.create(item);
+  
+  
+  ctx.body = {
+    code: 0,
+    data: result,
+  };
+  ctx.status = 201;
+});
+
+
+
+
 const app = new Koa();
 app
   .use(logger())
   .use(bodyParser())
   .use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
+  .use(cors());
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 90;
 async function bootstrap() {
   await initDB();
   app.listen(port, () => {
@@ -65,3 +91,5 @@ async function bootstrap() {
   });
 }
 bootstrap();
+
+cloud.init();
